1a729df3dccebd03768b518fc5ef471c78ab4356
Digno Alexander Familia <dalex2525@hotmail.com>
2020-11-15 03:30:03 -0400

    Base Api Architecture

---
+++ SPE.BL/Abstract/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SPE.DataModel.Abstract;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SPE.BL.Abstract
+{
+    public interface IBaseRepository<T> where T : class, IBaseEntity, new()
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> FindBy(Expression<Func<T, bool>> expression);
+        Task<T> Add(T entity);
+        Task<T> Update(T Entity);
+        Task<T> Delete(T entity);
+    }
+}
